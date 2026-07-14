69d11ce73ad9fbc5580f6736ec4b4334e90ec3c3
Helena Couto <helenacdos00@gmail.com>
2026-07-14 17:10:37 -0300

    feature: cria variaveis

---
+++ ListaCompras.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ListaCompras.h"
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+
+void leituraArquivos() {
+    ListaCompras *listaCompras;
+    Leitura *registro;
+    FILE *arquivo;
+
+}
---
+++ ListaCompras.h
@@ -0,0 +1,18 @@
+#ifndef LISTACOMPRAS_H
+#define LISTACOMPRAS_H
+#include <vector>
+#include <map>
+#include <string>
+
+typedef struct {
+    
+
+} ListaCompras;
+
+typedef struct {
+    char cod_cliente;
+    char cod_produto;
+    char nome_produto;
+} Leitura;
+
+#endif
