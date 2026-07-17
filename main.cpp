c8ca2f0acb94b74b3f9b7f60026846bf9d9d0b52
Helena Couto <helenacdos00@gmail.com>
2026-07-17 18:21:48 -0300

    feature: divide o programa em entregas

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ListaCompras.h"
 #include "Similaridade.h"
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Erro! Uso correto: %s <ARQUIVO_PARA_LEITURA>\n", argv[0]);
-        printf("Exemplo: %s dados/dados_venda_cluster_0.csv\n", argv[0]);
+    if (argc < 4) {
+        printf("Erro! Uso correto: %s <ARQUIVO_CSV> <ENTREGA> <K>\n", argv[0]);
+        printf("ENTREGA: 1 = ListaCompras | 2 = Similaridade | 3 = Recomendacao\n");
+        printf("Exemplo: %s dados/dados_venda_cluster_0.csv 2 5\n", argv[0]);
         return 1;
     }
 
     char *nome_arquivo = argv[1];
-    ListaCompras lista;
+    int entrega = atoi(argv[2]);
+    int k = atoi(argv[3]);
 
+    if (entrega > 3 || entrega < 1) {
+        printf("ENTREGA invalida. Use 1, 2 ou 3.\n");
+        return 1;
+    }    
+
+    ListaCompras lista;
     if (!criaListaCompras(nome_arquivo, &lista)) return 1;
-    testadorExibeProdutos(&lista);
+
+    if (entrega == 1) {
+        testadorExibeProdutos(&lista);
+        return 0;
+    }
 
     Similaridade sim;
     if (!criaMatrizCompras(&lista, &sim)) return 1;
@@ -22,8 +35,14 @@ int main(int argc, char *argv[]) {
     if (!criaVetorP(&lista, &sim)) return 1;
     if (!criaMatrizSimilaridade(&sim)) return 1;
 
-    testadorExibeSimilaridade(&sim, &lista, 0);
-    testadorExibeSimilaridade(&sim, &lista, 1);
+    if (entrega == 2) {
+        testadorExibeSimilaridade(&sim, &lista, 1);
+        testadorExibeSimilaridade(&sim, &lista, 4);
+        return 0;
+    }
 
-    return 0;
+    if (entrega == 3) {
+        printf("Entrega 3 (Recomendacao) ainda nao implementada.\n");
+        return 0;
+    }
 }
