0aac311c8536e090811ddf825ab2c5cc43081ca8
Helena Couto <helenacdos00@gmail.com>
2026-07-17 21:23:34 -0300

    fix: liberação de todas as memórias alocadas

--- Similaridade.cpp
+++ Similaridade.cpp
@@ -48,6 +48,17 @@ int criaTranspostaCompras(Similaridade *sim) {
     return true;
 }
 
+void liberaMatrizDouble(double **matriz, int linhas) {
+    if (matriz == NULL) return;
+
+    for (int i = 0; i < linhas; i++) {
+        free(matriz[i]);
+        matriz[i] = NULL;
+    }
+    free(matriz);
+    matriz = NULL;
+}
+
 int criaMatrizIntersecao(Similaridade *sim) {
     sim->I = (double**) malloc(sim->n * sizeof(double*));
     if (sim->I == NULL) return false;
@@ -65,6 +76,12 @@ int criaMatrizIntersecao(Similaridade *sim) {
             }
         }
     }
+
+    liberaMatrizDouble(sim->A, sim->n);
+    sim->A = NULL;
+    liberaMatrizDouble(sim->At, sim->m);
+    sim->At = NULL;
+
     return true;
 }
 
@@ -92,6 +109,12 @@ int criaMatrizSimilaridade(Similaridade *sim) {
             sim->S[i][j] = 1 - sim->I[i][j]/sim->P[i];
         }
     }
+
+    liberaMatrizDouble(sim->I, sim->n);
+    sim->I = NULL;
+    free(sim->P);
+    sim->P = NULL;
+
     return true;
 }
 
@@ -112,9 +135,9 @@ void testadorExibeSimilaridade (Similaridade *sim, ListaCompras *lista, int indC
             indSimilar = j;
             valorSimilaridade = similaridade;
         }
-     }
+    }
 
     cout << "\nCliente " << lista->vetorClientes[indSimilar] 
-        << " eh mais similar ao cliente " << lista->vetorClientes[indCliente];
+        << " eh o mais similar ao cliente " << lista->vetorClientes[indCliente];
     printf("\nSimilaridade: %.4f\n", valorSimilaridade);
 }
